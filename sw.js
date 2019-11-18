3a91052ebb0960b0a5bc8f6e924c984103932940
Alexandre Cousin <alexandre.cousin@spyrit.net>
2019-11-18 11:00:52 +0100

    SW with offline

---
+++ sw.js
@@ -0,0 +1,27 @@
+this.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open('v1').then(function(cache) {
+      return cache.addAll([
+        '/index.html',
+      ]);
+    })
+  );
+});
+
+this.addEventListener('fetch', function(event) {
+  event.respondWith(caches.match(event.request).then(function(response) {
+    if (response !== undefined) {
+      return response;
+    } else {
+      return fetch(event.request).then(function(response) {
+        let responseClone = response.clone();
+
+        caches.open('v1').then(function(cache) {
+          cache.put(event.request, responseClone);
+        });
+
+        return response;
+      });
+    }
+  }));
+});
